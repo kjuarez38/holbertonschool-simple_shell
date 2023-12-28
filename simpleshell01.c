44875956478ea9c740d3ef59e90bf9a8ce22307d
kjuarez38 <kaisjuarez@gmail.com>
2023-12-28 08:39:21 -0800

    line interpreter

---
+++ simpleshell01.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#define MAX_COMMAND_LENGTH 100
+
+/**
+ * main - ex the command of a simple shell
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int status;
+	char *command = NULL;
+	size_t len = 0;
+	pid_t pid;
+
+	while (1)
+	{
+		printf("simple_shell$ ");
+
+		if (getline(&command, &len, stdin) == -1)
+		{
+			printf("\n");
+			free(command);
+			break;
+		}
+		command[strcspn(command, "\n")] = '\0';
+		pid = fork();
+
+		if (pid < 0)
+		{
+			perror("Error forking process");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			if (execlp(command, command, (char *)NULL) == -1)
+			{
+				fprintf(stderr, "simple_shell: %s: command not found\n", command);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+			printf("\n");
+		}
+	}
+
+	free(command);
+	return (0);
+}
